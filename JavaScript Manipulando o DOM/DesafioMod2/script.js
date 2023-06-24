0458692744c0e7a5f3520f5d1c65f54d8e38b14e
Gustavo Silva <113210648+gustavo-log@users.noreply.github.com>
2023-06-23 21:26:14 -0300

    Commit full course

---
+++ JavaScript Manipulando o DOM/DesafioMod2/script.js
@@ -0,0 +1,5 @@
+const botao = document.querySelector("#calcular")
+const resultado = document.querySelector(".resultado")
+botao.addEventListener("click", ()=>{
+    resultado.innerHTML = "Fui Clicado"
+})
